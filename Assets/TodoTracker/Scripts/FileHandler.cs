2aec2744b8e4452f11e60fa660823e2ff67e0b7a
Cooper Saye <coopersaye@gmail.com>
2020-09-13 14:21:59 -0700

    Implement text file to todo parsing.

--- Assets/TodoTracker/Scripts/FileHandler.cs
+++ Assets/TodoTracker/Scripts/FileHandler.cs
@@ -1,29 +1,51 @@
-﻿using UnityEditor;
-using UnityEngine;
+﻿using System.Collections.Generic;
 using System.IO;
+using UnityEditor;
+using UnityEngine;
 
 namespace TodoTracker
 {
-    public class FileHandler
+    public class FileHandler : MonoBehaviour
     {
+        [Header("References")]
+        [SerializeField] private TodoList todoList = null;
+
         private static string path = "Assets/Resources/todo.txt";
+
+        private static List<string> todos = new List<string>();
+
+        public static void AddTodo(string todo)
+        {
+            todos.Add(todo);
+        }
 
         [MenuItem("Tools/Write File")]
         public static void WriteFile()
         {
             StreamWriter writer = new StreamWriter(path);
-            writer.WriteLine("testing");
+            foreach (string todo in todos) writer.WriteLine(todo);
             writer.Close();
-
-            // TextAsset file = (TextAsset)Resources.Load("todo");
         }
 
         [MenuItem("Tools/Read File")]
         public static void ReadFile()
         {
             StreamReader reader = new StreamReader(path);
-            Debug.Log(reader.ReadToEnd());
+            todos.Clear();
+            string[] list = reader.ReadToEnd().Split('\n');
+            foreach (string todo in list) todos.Add(todo);
             reader.Close();
+        }
+
+        private void Start()
+        {
+            ReadFile();
+            todoList.Initialize(todos.ToArray());
+        }
+
+        private void OnApplicationQuit()
+        {
+            WriteFile();
         }
     }
 }
--- Assets/TodoTracker/Scripts/TodoList.cs
+++ Assets/TodoTracker/Scripts/TodoList.cs
@@ -9,6 +9,34 @@ namespace TodoTracker
         [SerializeField] private GameObject todoItemPrefab = null;
         [SerializeField] private Transform contentTransform = null;
 
+        public void Initialize(string[] todos)
+        {
+            foreach (string todo in todos)
+            {
+                string title;
+                DateTime date;
+
+                try
+                {
+                    string[] titleDate = todo.Split('\\');
+                    title = titleDate[0];
+                    string[] dateDigits = titleDate[1].Split('/');
+                    
+                    int year = int.Parse(dateDigits[0]);
+                    int month = int.Parse(dateDigits[1]);
+                    int day = int.Parse(dateDigits[2]);
+                    date = new DateTime(year, month, day);
+                }
+                catch
+                {
+                    Debug.LogError($"Invalid todo {todo}!");
+                    continue;
+                }
+
+                CreateTodoItem(title, date);
+            }
+        }
+
         public void CreateTodoItem(string title, DateTime date)
         {
             GameObject todoObj = Instantiate(todoItemPrefab, transform.position, Quaternion.identity, contentTransform);
