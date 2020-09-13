cc06808b7e514c8eaa39be8af3df6b139e3b0250
Cooper Saye <coopersaye@gmail.com>
2020-09-13 13:59:26 -0700

    Implement text file reading and writing.

---
+++ Assets/TodoTracker/Scripts/FileHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+namespace TodoTracker
+{
+    public class FileHandler
+    {
+        private static string path = "Assets/Resources/todo.txt";
+
+        [MenuItem("Tools/Write File")]
+        public static void WriteFile()
+        {
+            StreamWriter writer = new StreamWriter(path);
+            writer.WriteLine("testing");
+            writer.Close();
+
+            // TextAsset file = (TextAsset)Resources.Load("todo");
+        }
+
+        [MenuItem("Tools/Read File")]
+        public static void ReadFile()
+        {
+            StreamReader reader = new StreamReader(path);
+            Debug.Log(reader.ReadToEnd());
+            reader.Close();
+        }
+    }
+}
